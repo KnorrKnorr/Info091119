ce57d9012b5b3bc232caad6b70ab9c4a64c257bf
KnorrKnorr <j.tpp@web.de>
2019-11-10 11:48:41 +0100

    some tests

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -9,20 +9,34 @@ int main()
 {
   cvector<int> MeinVektor1;
 
-  for (size_t i = 0; i < 1010; i++)
+  std::cout << "Intsaziierung :" << ": " << MeinVektor1.getSize() << std::endl;
+  std::cout << MeinVektor1.getCapacity() << std::endl;
+  std::cout << "Wert " << MeinVektor1[0] << std::endl;
+
+  for (size_t i = 0; i < 13; i++)
   {
     MeinVektor1.pushback(i);
     std::cout << "Durchgang " << i << ": " << MeinVektor1.getSize() << std::endl;
     std::cout << MeinVektor1.getCapacity() << std::endl;
-    std::cout << "Wert " << MeinVektor1[i+1] << std::endl;
+    std::cout << "Wert " << MeinVektor1[i] << std::endl;
   }
 
-  for (size_t i = 0; i < 10; i++)
+  MeinVektor1.resize(100);
+  std::cout << "resize(10)" << ": " << MeinVektor1.getSize() << std::endl;
+  std::cout << MeinVektor1.getCapacity() << std::endl;
+
+  MeinVektor1.resize(100);
+  std::cout << "resize(10)" << ": " << MeinVektor1.getSize() << std::endl;
+  std::cout << MeinVektor1.getCapacity() << std::endl;
+
+
+
+  /*for (size_t i = 0; i < 10; i++)
   {
     MeinVektor1.popback();
     std::cout << "Durchgang " << i << ": " << MeinVektor1.getSize() << std::endl;
     std::cout << MeinVektor1.getCapacity() << std::endl;
     std::cout << "Wert " << MeinVektor1[i+1] << std::endl;
-  }
+  }*/
 }
 
